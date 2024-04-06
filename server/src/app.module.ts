604d1ac3f7013ca6a217f2966108694065ce36ca
Francyn Macadangdang <izaya.orihara0011@gmail.com>
2024-04-06 15:40:26 +0800

    initial crud setup for doctor, nurse, patient

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { NurseModule } from './nurse/nurse.module';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
   imports: [
@@ -10,6 +13,9 @@ import { AuthModule } from './auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    DoctorModule,
+    NurseModule,
+    PatientModule,
   ],
   controllers: [],
   providers: [],
